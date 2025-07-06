4990bd6ea7759a2ee7483533a6c8b2683b70aabe
Sanzay7 <sanjayakhanal1117@gmail.com>
2025-07-06 19:35:53 +0545

    Add project files.

---
+++ SignalRInAspNetCore/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using System.Security;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRInAspNetCore
+{
+      public class ChatHub : Hub
+        {
+            public async Task SendMessage(string user, string message, string groupName)
+            {
+                await Clients.Group(groupName).SendAsync("ReceivedMessage", user, message);
+            }
+            public async Task BroadCastMessage(string user, string message)
+            {
+                await Clients.All.SendAsync(user, message);
+        }
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", groupName,"");
+        }
+        public async Task LeaveGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} Left {groupName}","system");
+        }
+    }
+
+
+
+}
---
+++ SignalRInAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalRInAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
